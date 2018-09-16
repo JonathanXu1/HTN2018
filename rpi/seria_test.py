f428cf21bff1c736e48849c49090f3f52e60ffbb
Jonathan <jonathanxu25@gmail.com>
2018-09-16 10:58:15 -0400

    ffdf

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -10,13 +10,31 @@ try:
 except ImportError:
     import _thread as thread
 import time
+import socket_io as io
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(("10.21.62.43", 8080))
-sock.listen(2)
-(client,(ip,port))=sock.accept()
-time.sleep(1)
-client.send("HELLO")
+
+class Server(io.Server):
+    def on_connect(self, client):
+        print
+        client, 'connected'
+        self.broadcast(str(client) + ' connected')
+        print
+        'there are now', len(self.clients), 'clients'
+
+    def on_message(self, client, message):
+        print
+        client, 'sent', message
+        client.send(message)
+
+    def on_disconnect(self, client):
+        print
+        client, 'disconnected'
+        self.broadcast(str(client) + ' disconnected')
+        print
+        'there are now', len(self.clients), 'clients'
+
+
+Server().listen(5000)
 
 # ser = serial.Serial('/dev/ttyACM0',9600)
 
