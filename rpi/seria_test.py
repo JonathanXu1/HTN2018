96993f0a3a235d8d31e386b2b92ac989e6f953d6
Jonathan <jonathanxu25@gmail.com>
2018-09-16 09:52:38 -0400

    cust

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -13,18 +13,18 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 
+async def hello():
+    async with websockets.connect(
+            'ws://10.21.62.43:8765') as websocket:
+        name = input("What's your name? ")
 
+        await websocket.send(name)
+        print(f"> {name}")
 
-async def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
 
-start_server = websockets.serve(time, '10.21.62.43', 5678)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(hello())
 
 
 while True:
