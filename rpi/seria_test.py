797479503465d8a2ac6d264e8d5f046551541935
Jonathan <jonathanxu25@gmail.com>
2018-09-16 10:49:13 -0400

    fdsfs

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -3,25 +3,26 @@ import socket
 import asyncio
 import datetime
 import random
-import websockets
+import socket
+
 try:
     import thread
 except ImportError:
     import _thread as thread
 import time
 
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind(("10.21.62.43", 80))
+sock.listen(2)
+(client,(ip,port))=sock.accept()
+time.sleep(1)
+client.send("HELLO")
+
 # ser = serial.Serial('/dev/ttyACM0',9600)
 
-async def hello():
-    async with websockets.connect(
-            'ws://10.21.62.43:8765') as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-
-asyncio.get_event_loop().run_until_complete(hello())
 
 
-while True:
-    data = ser.readline()
-    print(data)
+#
+# while True:
+#     data = ser.readline()
+#     print(data)
