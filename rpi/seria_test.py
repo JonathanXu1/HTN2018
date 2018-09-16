52323d8167b9160f124d736f27e4ac66399fac2b
Jonathan <jonathanxu25@gmail.com>
2018-09-16 11:20:44 -0400

    gf

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -1,46 +1,12 @@
-import serial
-import socket
-import asyncio
-import datetime
-import random
-import socket
-
-try:
-    import thread
-except ImportError:
-    import _thread as thread
-import time
-import socket_io as io
+from threading import Thread
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 
-class Server(io.Server):
-    def on_connect(self, client):
-        print
-        client, 'connected'
-        self.broadcast(str(client) + ' connected')
-        print
-        'there are now', len(self.clients), 'clients'
-
-    def on_message(self, client, message):
-        print
-        client, 'sent', message
-        client.send(message)
-
-    def on_disconnect(self, client):
-        print
-        client, 'disconnected'
-        self.broadcast(str(client) + ' disconnected')
-        print
-        'there are now', len(self.clients), 'clients'
+class WebSocketHandler(WebSocket):
+    def handleConnected(self):
+        while True:
+            self.sendMessage("hi")
 
 
-Server().listen(5000)
-
-# ser = serial.Serial('/dev/ttyACM0',9600)
-
-
-
-#
-# while True:
-#     data = ser.readline()
-#     print(data)
+server = SimpleWebSocketServer("0.0.0.0", 8000, WebSocketHandler)
+Thread(target=server.serveforever, daemon=True).start()
