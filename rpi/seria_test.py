f3222c0be42ca9cb17ce3e1a6436b7c36ac8dced
Jonathan <jonathanxu25@gmail.com>
2018-09-16 10:16:02 -0400

    fdgfdgd

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -19,11 +19,7 @@ async def hello():
         name = input("What's your name? ")
 
         await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
+       
 asyncio.get_event_loop().run_until_complete(hello())
 
 
