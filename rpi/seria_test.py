b1870bccee8ffa2bc56d896c073a1720323fa8d9
Jonathan <jonathanxu25@gmail.com>
2018-09-16 08:09:28 -0400

    fasdfas

--- rpi/seria_test.py
+++ rpi/seria_test.py
@@ -3,6 +3,6 @@ import serial
 ser = serial.Serial('/dev/ttyACM0',9600)
 
 while True:
-	bytesToRead = ser.inWaiting()
-	data = ser.read(bytesToRead)
-	print(data)
+    bytesToRead = ser.inWaiting()
+    data = ser.read(bytesToRead)
+    print(data)
